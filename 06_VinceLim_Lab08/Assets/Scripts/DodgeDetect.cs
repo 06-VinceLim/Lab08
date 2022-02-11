13ce1eb814e84f13805c5fe1ac85be9123f504b8
06-VinceLim <happyvince999@gmail.com>
2022-02-11 18:33:25 +0800

    Unity Update 4
    Update score when the player dodge the obstacle.

--- 06_VinceLim_Lab08/Assets/Scripts/DodgeDetect.cs
+++ 06_VinceLim_Lab08/Assets/Scripts/DodgeDetect.cs
@@ -1,34 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class DodgeDetect : MonoBehaviour
 {
-    public float speed;
+    int dodge;
+
+    public Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
-
-
-
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Obstacle")
         {
-            SceneManager.LoadScene("GameOverScene");
+            dodge++;
+
+            scoreText.text = "Score : " + dodge;
+
+            Destroy(collision.gameObject);
+
         }
     }
 }
