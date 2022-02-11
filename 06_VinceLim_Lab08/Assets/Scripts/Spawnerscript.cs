43088c10169c1f327e24fca614a152bda688bd25
06-VinceLim <happyvince999@gmail.com>
2022-02-11 18:05:30 +0800

    Unity Update 2
    Restricted player from going outside the screen and spawner spawning more than 1 block, there is 3 blocks it spawn now.

--- 06_VinceLim_Lab08/Assets/Scripts/Spawnerscript.cs
+++ 06_VinceLim_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
+    public GameObject[] SpawnObject;
+
+    int randomBlock;
+
     float PositionY;
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("randomNumber", 1, 1);
+
         InvokeRepeating("SpawnObjects", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
 
 
     }
@@ -25,6 +30,11 @@ public class Spawnerscript : MonoBehaviour
     {
         PositionY = Random.Range(4, -4f);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject, transform.position, transform.rotation);
+        Instantiate(SpawnObject[randomBlock], transform.position, transform.rotation);
+    }
+
+    void randomNumber()
+    {
+        randomBlock = Random.Range(0, 3);
     }
 }
